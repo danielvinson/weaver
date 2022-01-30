03b876b0dd1d861f6b0d1b3b4ea8733ccbd24c94
Daniel Vinson <daniel@gomerits.com>
2022-01-30 01:31:29 -0800

    Update blitz downloader for tournament

--- util/blitzdownload/download.py
+++ util/blitzdownload/download.py
@@ -11,10 +11,11 @@ act_ids = {
         "act1": "2a27e5d2-4d30-c9e2-b15a-93b8909a442c",
         "act2": "4cb622e1-4244-6da3-7276-8daaf1c01be2",
         "act3": "a16955a5-4ad0-f761-5e9e-389df1c892fb",
-    }
+    },
+    "episode4": {"act1": "573f53ac-41a5-3a7d-d9ce-d6a6298e5704"},
 }
 
-boomer_match_filename = "noober_match_ids.txt"
+boomer_match_filename = "charity_tournament_match_ids.txt"
 
 
 def download_match(match_id, act_id, type):
@@ -23,10 +24,18 @@ def download_match(match_id, act_id, type):
     return res
 
 
-def download_boomer_matches():
-    act1 = act_ids["episode3"]["act1"]
-    act2 = act_ids["episode3"]["act2"]
-    act3 = act_ids["episode3"]["act3"]
+def download_matches():
+    acts = [
+        # act_ids["episode3"]["act1"],
+        # act_ids["episode3"]["act2"],
+        # act_ids["episode3"]["act3"],
+        act_ids["episode4"]["act1"],
+    ]
+    match_types = [
+        "subject",
+        # "puuid"
+    ]
+
     with open(boomer_match_filename, "r") as infile:
         for line in infile:
             match_id = line.strip()
@@ -34,36 +43,19 @@ def download_boomer_matches():
                 continue
             with open(f"{data_dir}{match_id}.json", "w") as outfile:
                 print(f"downloading {match_id}")
-                match_type = "subject"
-                res = download_match(match_id, act1, "subject")
 
-                if len(res.content) == 0:
+                for act in acts:
+                    res = download_match(match_id, act, "subject")
+                    if len(res.content) > 0:
+                        break
                     time.sleep(2)
-                    res = download_match(match_id, act2, "subject")
-
-                if len(res.content) == 0:
-                    time.sleep(2)
-                    res = download_match(match_id, act3, "subject")
-
-                if len(res.content) == 0:
-                    time.sleep(2)
-                    match_type = "puuid"
-                    res = download_match(match_id, act1, "puuid")
-
-                if len(res.content) == 0:
-                    time.sleep(2)
-                    res = download_match(match_id, act2, "puuid")
-
-                if len(res.content) == 0:
-                    time.sleep(2)
-                    res = download_match(match_id, act3, "puuid")
 
                 if len(res.content) == 0:
                     print(f"Couldn't find match {match_id}")
                 else:
-                    json.dump({"match_type": match_type, "data": res.json()}, outfile)
+                    json.dump({"match_type": "subject", "data": res.json()}, outfile)
 
-                time.sleep(5)
+                time.sleep(2)
 
 
-download_boomer_matches()
+download_matches()
