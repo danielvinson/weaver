07fb31544d75d61eba77f1ba742d14deb8fd4247
Daniel Vinson <daniel@gomerits.com>
2022-03-23 13:36:22 -0700

    Add structures for season 3 league data

---
+++ frontend/src/data/boomerSeason3/academyMatches.tsx
@@ -0,0 +1,8 @@
+export const groupA = {
+  week1: [],
+  week2: [],
+  week3: [],
+  week4: [],
+  week5: [],
+  week6: [],
+};
---
+++ frontend/src/data/boomerSeason3/championshipMatches.tsx
@@ -0,0 +1,35 @@
+export const groupA = {
+  week1: [],
+  week2: [],
+  week3: [],
+  week4: [],
+  week5: [],
+  week6: [],
+};
+
+export const groupB = {
+  week1: [],
+  week2: [],
+  week3: [],
+  week4: [],
+  week5: [],
+  week6: [],
+};
+
+export const groupC = {
+  week1: [],
+  week2: [],
+  week3: [],
+  week4: [],
+  week5: [],
+  week6: [],
+};
+
+export const groupD = {
+  week1: [],
+  week2: [],
+  week3: [],
+  week4: [],
+  week5: [],
+  week6: [],
+};
