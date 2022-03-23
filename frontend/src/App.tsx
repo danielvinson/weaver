9043130a83c64895ca4b5333f075a95c5c98037e
Daniel Vinson <daniel@gomerits.com>
2022-03-23 13:36:03 -0700

    Retire StatChart page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Home } from "./screens/Home";
 import { MatchDetail } from "./screens/MatchDetail";
 import { MatchHistory } from "./screens/MatchHistory";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import { StatChart } from "./components/MatchStatChart/StatChart";
 
 export const ROUTES = {
   detail: { key: "detail", name: "Match", path: "/match/:actId/:matchId" },
@@ -13,18 +12,12 @@ export const ROUTES = {
     path: "/matchHistory/:playerName/:playerTag",
   },
   home: { key: "home", name: "Home", path: "/" },
-  statChart: { key: "statChart", name: "Stat Chart", path: "/statChart" },
 };
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path={ROUTES.statChart.path}>
-          <DefaultLayout activeRoute="starChart">
-            <StatChart />
-          </DefaultLayout>
-        </Route>
         <Route path={ROUTES.detail.path}>
           <DefaultLayout activeRoute="detail">
             <MatchDetail />
