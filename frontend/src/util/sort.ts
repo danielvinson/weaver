66243fc85c8a2ee6e72e5db9996a96ae5bbebb8a
Daniel Vinson <daniel@gomerits.com>
2022-05-03 13:59:31 -0700

    Add sorting by strings

--- frontend/src/util/sort.ts
+++ frontend/src/util/sort.ts
@@ -8,18 +8,24 @@ export function sortData<T>(
   return [...data].sort((a, b) => {
     const val1 = a[sortBy];
     const val2 = b[sortBy];
+    if (typeof val1 === "string" && typeof val2 === "string") {
+      return direction === "Descending"
+        ? val2.localeCompare(val1)
+        : val1.localeCompare(val2);
+    }
+
     if (typeof val1 === "number" && typeof val2 === "number") {
       return direction === "Descending" ? val2 - val1 : val1 - val2;
-    } else {
-      if (val1 > val2) {
-        return 1;
-      }
-
-      if (val2 > val1) {
-        return -1;
-      }
-
-      return 0;
     }
+
+    if (val1 > val2) {
+      return 1;
+    }
+
+    if (val2 > val1) {
+      return -1;
+    }
+
+    return 0;
   });
 }
