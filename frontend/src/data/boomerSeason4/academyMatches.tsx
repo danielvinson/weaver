bade9204fed0a35336178191fa8e45a97ccb7dc4
Daniel Vinson <daniel@gomerits.com>
2022-06-17 13:11:39 -0700

    Add more challenger and academy matches

--- frontend/src/data/boomerSeason4/academyMatches.tsx
+++ frontend/src/data/boomerSeason4/academyMatches.tsx
@@ -2,8 +2,15 @@ export const groupA = {
   week1: [
     "086cd822-262c-48a8-9b0c-8cb08814d932",
     "dad87e21-28e3-4e67-a4c3-d72902883b42",
+    "d0fd9229-af83-4227-9404-1c518ca9bb5b",
+    "a6d013de-3c15-438d-9527-779ad477c688",
   ],
-  week2: [],
+  week2: [
+    "2f8d79cf-90ce-478a-b4dd-442a084d563a",
+    "5da313b6-4c63-4792-b2dd-7c6b84bb1dac",
+    "7ef63d85-22a5-4c7e-bd9d-eca4f213adc6",
+    "a09e4a37-3c23-47e5-a6ac-30a7c4266c23",
+  ],
   week3: [],
   week4: [],
   week5: [],
