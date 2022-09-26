8236c840a3ac70ffb717480e5d35ed459a1c5101
Daniel Vinson <daniel@gomerits.com>
2022-09-26 14:22:37 -0700

    Add initial RDV academy matches

--- frontend/src/data/rendezvous/academy.ts
+++ frontend/src/data/rendezvous/academy.ts
@@ -1,5 +1,10 @@
 export const matches = {
-  week1: [],
+  week1: [
+    "43e7d15e-4927-456f-b1fb-3e116daace08",
+    "15170f16-118a-4b57-8b71-d8208a7ffbfa",
+    "6df86302-1ebe-4e6d-a410-5d7959798bf6",
+    "e2a4ed02-031d-4703-a3bb-655b9cb73e90",
+  ],
   week2: [],
   week3: [],
   week4: [],
